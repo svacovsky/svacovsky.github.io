92d707145e5e1ef8ee74ce9bfb1d493d8acc2451
Sarah Vacovsky <sarahvacovsky@gmail.com>
2018-07-23 21:27:00 -0400

    Adding frogger code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# frogger
+frontend-nanodegree-arcade-game
+===============================
+To run the application, simply open the index.html.
+
+To play the game, use the arrow keys to move your character across the path to the water. Once you reach the water you win! If you hit a bug you have to restart so watch out!
---
+++ js/app.js
@@ -0,0 +1,129 @@
+// Enemies our player must avoid
+let Enemy = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.reset();
+};
+
+function getRandomFromSet(set)
+{
+    var rndm = Math.floor(Math.random() * set.length);
+    return set[rndm];
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    const temp = this.speed * dt;
+    if((this.x + temp) > 505){
+      this.reset();
+    }else{
+      this.x += (this.speed * dt);
+    }
+    const enemyCoords = this.determineCoordinates(dt);
+    const playerCoords = player.determineCoordinates();
+
+    if(enemyCoords[0] == playerCoords[0] && enemyCoords[1] == playerCoords[1]){
+      player.reset();
+    }
+  //  this.y += this.y * dt;
+};
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Enemy.prototype.determineCoordinates = function(dt){
+  let coords = [];
+  coords[0] = Math.round(this.x / 101);
+  coords[1] = ((this.y + 25) / 83 );
+  return coords;
+}
+
+Enemy.prototype.reset = function(){
+    this.sprite = 'images/enemy-bug.png';
+    this.x = 1;
+    this.y = getRandomFromSet([(1*83)-25,(2*83)-25, (3*83)-25]);
+    this.speed = getRandomInt(50,500);
+}
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+let Player = function(){
+  this.reset();
+};
+
+Player.prototype.update = function(){
+
+};
+
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key){
+  console.log(key);
+  if(key === "left" && this.x > 0){
+    this.x = this.x - 101;
+  }else if (key === "right" && this.x < 404){
+    this.x = this.x + 101;
+  }else if(key === "up" && this.y > 58){
+    this.y = this.y - 83;
+  }else if(key === "down" && this.y < 390){
+    this.y = this.y + 83;
+  }else if (key === "up" && this.y <= 58){
+    this.reset();
+    alert("You win!");
+  }
+};
+
+Player.prototype.determineCoordinates = function(){
+  let coords = [];
+  coords[0] = Math.round(this.x / 101);
+  coords[1] = Math.round((this.y) / 83 );
+  return coords;
+};
+
+Player.prototype.reset = function(){
+    this.sprite = 'images/char-boy.png';
+    this.x = 2*101;
+    this.y = (4*83)-25;
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+let allEnemies = new Array();
+for(let x = 0; x < 3; x ++){
+  allEnemies[x] = new Enemy();
+  console.log(allEnemies[x]);
+}
+// Place the player object in a variable called player
+let player = new Player();
+console.log(player);
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
